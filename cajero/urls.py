e53653f00553a66c32294ba4f57cdd6e0944957a
Martin Moreira <172557112+mimoreirac@users.noreply.github.com>
2025-01-25 10:18:58 -0500

    Añade paginas de transacciones

--- cajero/urls.py
+++ cajero/urls.py
@@ -5,4 +5,8 @@ app_name = 'cajero'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('depositos/', views.deposit, name='deposit'),
+    path('retiros/', views.withdrawal, name='withdrawal'),
+    path('pagos/', views.payment, name='payment'),
+    path('saldo/', views.check_balance, name='check_balance'),
 ]
--- cajero/views.py
+++ cajero/views.py
@@ -5,3 +5,15 @@ from django.template import loader
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def deposit(request):
+    return render(request, 'depositos.html')
+
+def withdrawal(request):
+    return render(request, 'retiros.html')
+
+def payment(request):
+    return render(request, 'pagos.html')
+
+def check_balance(request):
+    return render(request, 'saldo.html')
